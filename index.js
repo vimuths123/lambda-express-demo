ad068e36cf73c99f93914e6749598618f230a151
vimuth <vimuthtweet@gmail.com>
2026-05-31 13:30:14 +0530

    Git deploy test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const serverless = require("serverless-http");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World from Express!");
+  res.send("Hello World from Express deployed from git!");
 });
 
 module.exports.handler = serverless(app);
