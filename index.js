9d20695649ee3ce0b4e7930d072bb88e3fe52688
vimuth <vimuthtweet@gmail.com>
2026-05-31 13:32:03 +0530

    Revert it

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const serverless = require("serverless-http");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World from Express deployed from git!");
+  res.send("Hello World from Express");
 });
 
 module.exports.handler = serverless(app);
